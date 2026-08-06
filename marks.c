96c7210a276991df506ccbadd487c00ca80e5a5a
VedantKurhade07 <kurhadevedant2006@gmail.com>
2026-08-06 11:03:52 +0530

    Student information

---
+++ marks.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum;
+	printf("Enter Number1 :");
+	scanf("%d",&num1);
+	printf("Enter Number2 :");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("Addition of given numbers are :%d",sum);
+	return 0;
+}
---
+++ student.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello Student");
+	printf("TY Bsc.CS");
+}
